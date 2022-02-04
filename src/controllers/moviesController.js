d54795cb6d794c3f3d10047b6656f6d89f1f63e4
luis crescente <luis-94@live.com.ar>
2022-02-04 00:38:30 -0300

    endpoint movies detail

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -4,21 +4,74 @@ const path = require('path');
 
 let moviesController = {
 
-    list:(req,res) =>{
-        db.Movies
-        .findAll({include:[{association: 'genres'}]})
-       .then(movies => {
-           return res.status(200).json({
-               total: movies.length,
-               status:200,
-               
-           })
-       }).catch(function(err){
+     /***** Busca todas las peliculas *****/
 
-           console.log(err);
-       })
+    list: async (req,res) =>{
+        let allMovies = await db.Movies.findAll({
+            include: [
+                {association: 'genres'},
+                {association: 'characters'}
+            ]
+        });   
+            try {
+                getMovies = allMovies.map(movie => {  
+                    movie = {
+                        id: movie.dataValues.id,
+                        title: movie.dataValues.title,
+                        release_date: movie.dataValues.release_date,
+                        rating: movie.dataValues.rating,
+                        genres: movie.genres.dataValues.name,
+                        //characters:characters
+                        image:`http://localhost:3005/img/moviesImg/${movie.dataValues.image}`
+                    };
+                    return movie;
+                });
+                res.status(200).json({
+                    totalMovies: getMovies.length,
+                    movies: getMovies,
+                    status: 200,
+                });
+            } catch (err) {
+                console.log(err);
+        
+            };
+        },
+
+    
+    /***** Busca una pelicula por su ID *****/
+
+    detail: async (req,res) =>{
+
+        let oneMovie = await db.Movies.findByPk(req.params.id, {
+            include: [
+                {association: 'genres'},
+                {association: 'characters'}
+            ]
+        }); 
+        try{
+                delete oneMovie.dataValues.genre_id;      
+                let characters = [];                
+                    oneMovie.characters.forEach(character => {
+                    characters.push(character.dataValues.name);
+                });
+
+                let movie = {
+                    id: oneMovie.id,
+                    title: oneMovie.title,
+                    rating: oneMovie.rating,
+                    image:`http://localhost:3005/img/moviesImg/${oneMovie.dataValues.image}`,
+                    release_date: oneMovie.release_date,
+                    characters: characters,
+                    genres: oneMovie.genres.dataValues.name
+                };
+                res.status(200).json({
+                    movies: movie,
+                    status: 200,
+                });
+        }catch (err) {
+                console.log(err);
+            };
     }
-
 }
 
 module.exports = moviesController;
