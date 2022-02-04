a2c92e1e508e72f1c4a9228b894a64bc404865da
luis crescente <luis-94@live.com.ar>
2022-02-04 00:29:18 -0300

    agrego nuevo endpoint movie detail

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/moviesController')
 
-
+// Listado de todas las peliculas 
 router.get('/',controller.list);
+
+// Obtengo las peliculas por id
+router.get('/:id', controller.detail);
 
 module.exports = router
