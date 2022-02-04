12f9d8980c9ad21c632bd38c4c80067712923a28
luis crescente <luis-94@live.com.ar>
2022-02-04 00:43:28 -0300

    agrego scripts para crear y llenar base de datos

--- src/database/script/data.sql
+++ src/database/script/data.sql
@@ -1,92 +1,16 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema disney_world
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema disney_world
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `disney_world` DEFAULT CHARACTER SET utf8 ;
-USE `disney_world` ;
-
--- -----------------------------------------------------
--- Table `disney_world`.`characters`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `disney_world`.`characters` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `history` VARCHAR(45) NULL DEFAULT NULL,
-  `weight` INT(11) NULL DEFAULT NULL,
-  `age` INT(11) NULL DEFAULT NULL,
-  `image` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+/* Genres */
+INSERT INTO `disney_world`.`genres` (`id`, `name`) VALUES ('1', 'Fantasia ');
+INSERT INTO `disney_world`.`genres` (`id`, `name`) VALUES ('2', 'Aventuras');
+INSERT INTO `disney_world`.`genres` (`id`, `name`) VALUES ('3', 'Aventuras');
 
 
--- -----------------------------------------------------
--- Table `disney_world`.`genres`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `disney_world`.`genres` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+/* Character */
+INSERT INTO `disney_world`.`characters` (`id`, `name`, `history`, `weight`, `age`) VALUES ('1', 'Mickey Mouse', 'Mickey Mouse es un personaje ficticio estadounidense de la serie del mismo nombre, emblema de la compañía Disney', '70', '93');
 
 
--- -----------------------------------------------------
--- Table `disney_world`.`movies`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `disney_world`.`movies` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NULL DEFAULT NULL,
-  `rating` INT(11) NULL DEFAULT NULL,
-  `release_date` DATE NULL DEFAULT NULL,
-  `image` VARCHAR(255) NULL DEFAULT NULL,
-  `genre_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `genre_id_idx` (`genre_id` ASC),
-  CONSTRAINT `genre_id`
-    FOREIGN KEY (`genre_id`)
-    REFERENCES `disney_world`.`genres` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8;
+/* Movies */
+INSERT INTO `disney_world`.`movies` (`id`, `title`, `rating`, `release_date`, `genre_id`) VALUES ('1', 'La hora de la sinfonía', '5', '1942-03-20', '1');
 
 
--- -----------------------------------------------------
--- Table `disney_world`.`character_movie`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `disney_world`.`character_movie` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `character_id` INT(11) NOT NULL,
-  `movie_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `character_id_idx` (`character_id` ASC),
-  INDEX `movie_id_idx` (`movie_id` ASC),
-  CONSTRAINT `character_id`
-    FOREIGN KEY (`character_id`)
-    REFERENCES `disney_world`.`characters` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `movie_id`
-    FOREIGN KEY (`movie_id`)
-    REFERENCES `disney_world`.`movies` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/* Tabla intermedia character-movie*/
+INSERT INTO `disney_world`.`character_movie` (`id`, `character_id`, `movie_id`) VALUES ('1', '1', '1');
