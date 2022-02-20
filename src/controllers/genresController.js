f3e3b8e35b3cc39bdecfdbfd454c9f731240e443
kany west <luis-94@live.com>
2022-02-20 16:32:59 -0300

    agregando endpoint de genero

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,25 +1,71 @@
 let db = require('../database/models')
 const path = require('path');
-const Op = db.Sequelize.Op
+
 
 
 let genresController = {
 
-    list:(req,res) =>{
-         db.Genre
-         .findAll()
-        .then(genres => {
-            return res.status(200).json({
-                total: genres.length,
-                status:200,
-                
-            })
-        }).catch(function(err){
+     /***** Busca todos los generos *****/
 
-            console.log(err);
+    list: async (req,res) =>{
+        let allGenres = await db.Genre.findAll({
+            include: [
+                {association: 'movies'}
+            ]
         })
-     }
+            try{
+                getGenres = allGenres.map(genre =>{
+                    genre ={
+                        id: genre.dataValues.id,
+                        name: genre.dataValues.name,
+                        image:`http://localhost:3005/img/genreImg/${genre.dataValues.image}`,
 
+                    }
+                    return genre
+                })
+                    res.status(200).json({
+                        genres: getGenres,
+                        status:200,
+                    });
+            } catch (err) { console.log(err) };
+        },
+
+
+
+     /***** Busca los generos por ID *****/
+    
+    detail: async (req,res) =>{
+        let oneGenre = await db.Genre.findByPk(req.params.id)
+
+        try{
+            let genre ={
+                id: oneGenre.dataValues.id,
+                name: oneGenre.dataValues.name,
+                image:`http://localhost:3005/img/genreImg/${oneGenre.dataValues.image}`,
+            }
+            res.status(200).json({
+                genre : genre,
+                status:200,
+            })
+        }catch (err) { console.log(err) };
+    },
+    
+    
+    /***** Creacion de genero *****/
+
+    create: (req,res) =>{
+        db.Genre.create({
+            ...req.body,
+            image: req.file.filename,
+        })
+        .then( genre =>{
+            return res.status(200).json({
+                data: genre,
+                status:200,
+                created: 'genero creado',
+            })
+        }).catch(error=>console.log(error))
+    }
 }
 
 module.exports = genresController;
--- src/routes/genres.js
+++ src/routes/genres.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/genresController')
+const controller = require('../controllers/genresController');
+const upload = require('../middleware/genreImg');
 
-
+// Listado de genero
 router.get('/',controller.list);
+
+// Obtengo todos los generos por id
+router.get('/:id', controller.detail);
+
+// Creacion de generos
+router.post('/', upload.single('image') ,controller.create);
 
 module.exports = router
